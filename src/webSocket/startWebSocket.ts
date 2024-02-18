6da4bdd30eeb39659dd38b71e862faaa1e8bd391
Mikalai Rusakovich <kolyos83@mail.ru>
2024-02-19 02:36:07 +0300

    feat: change startWebSocket.ts

--- src/webSocket/startWebSocket.ts
+++ src/webSocket/startWebSocket.ts
@@ -1,13 +1,28 @@
 import WebSocket from 'ws';
+import { TYPES_ENUM } from '../interfaces/types.model';
+import { regHandling } from '../requestsHandling/regHandling/regHandling';
+import { createRoomHandling } from '../requestsHandling/createRoomHandling';
+import { IReg } from '../requestsHandling/regHandling/reg.model';
 
-export function startWebSocket(port: string) {
-  const wss = new WebSocket.Server({ port: +port });
+const wss = new WebSocket.Server({
+  port: +(process.env.WS_PORT || '3000'),
+});
 
+export function startWebSocket() {
   wss.on('connection', ws => {
     console.log('Client connected');
 
     ws.on('message', message => {
-      console.log('Received message:', message.toString());
+      const request: IReg = JSON.parse(message.toString());
+      switch (request.type) {
+        case TYPES_ENUM.REG:
+          ws.send(JSON.stringify(regHandling(request)));
+          break;
+        case TYPES_ENUM.CREATE_ROOM:
+          ws.send(JSON.stringify(createRoomHandling(request)));
+          break;
+      }
+      console.log('Received message:', request);
     });
   });
 }
